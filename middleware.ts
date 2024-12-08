cdf2256600a8773e09ba2c941fd637e9f7aae19c
ZhiYong Jie <823425525@qq.com>
2024-12-08 20:49:12 +0800

    chore: update demo

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
-import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
 
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"], // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  // 设置路由匹配规则，决定哪些路由需要进行授权检查
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'], // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 };
