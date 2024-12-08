7bd91058d450b3b527b6c91493621c399235ee78
ZhiYong Jie <823425525@qq.com>
2024-12-08 21:00:07 +0800

    chore: fix prettier

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
-import { codeInspectorPlugin } from "code-inspector-plugin";
+import type { NextConfig } from 'next';
+import { codeInspectorPlugin } from 'code-inspector-plugin';
 
 const nextConfig: NextConfig = {
   webpack: (config, { dev, isServer }) => {
-    config.plugins.push(codeInspectorPlugin({ bundler: "webpack" }));
+    config.plugins.push(codeInspectorPlugin({ bundler: 'webpack' }));
     return config;
   },
 };
