5894351a30795115c388275761db24e254e96d57
ZhiYong Jie <823425525@qq.com>
2024-12-14 21:04:00 +0800

    feat: update

--- app/not-found.tsx
+++
@@ -1,5 +0,0 @@
-'use client';
-
-export default function RootNotFound() {
-  return 'RootNotFound';
-}
--- services/actions.ts
+++ services/actions.ts
@@ -5,7 +5,11 @@ import { AuthError } from 'next-auth';
 
 export async function authenticate(prevState: string | undefined, formData: FormData) {
   try {
-    await signIn('credentials', formData);
+    const result = await signIn('credentials', {
+      ...Object.fromEntries(formData),
+      redirect: false,
+    });
+    return result;
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
